034e1a6a5d4f35834524ce133e83441dc7e05ef1
Felix C. Morency <1102868+fmorency@users.noreply.github.com>
2025-07-02 16:39:04 -0400

    fix: tests

---
+++ tests/docker/push.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+if ! curl --data-binary @/geo.prom http://pushgateway_test:9091/metrics/job/geo
+then
+  echo "Common fixture failed"
+  exit 1
+fi
+
+if ! curl --data-binary @/common.prom http://pushgateway_test:9091/metrics/job/common
+then
+  echo "Common fixture failed"
+  exit 1
+fi
+touch /tmp/SUCCESS
+tail -f /dev/null
